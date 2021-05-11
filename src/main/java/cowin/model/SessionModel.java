591c1357ca36bf175528169a2dd74c5ccc730596
amanjadon54 <amanjadon54@gmail.com>
2021-05-11 23:49:49 +0530

    Core: reduced attributes unused

--- src/main/java/cowin/model/SessionModel.java
+++ src/main/java/cowin/model/SessionModel.java
@@ -4,19 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class SessionModel {
 
-    private String session_id;
     private String date;
     private Integer available_capacity;
     private Integer min_age_limit;
     private String vaccine;
-    private List<String> slots;
 
     public Integer getAvailable_capacity() {
         return available_capacity;
