fd56ac6300d984a6cabfce7c579f7cf852a2fb55
amanjadon54 <amanjadon54@gmail.com>
2021-05-11 21:46:03 +0530

    Core: Run cowin for local

--- src/main/java/cowin/CowinApp.java
+++ src/main/java/cowin/CowinApp.java
@@ -1,27 +1,12 @@
 package cowin;
 
-import com.amazonaws.services.lambda.runtime.Context;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Map;
-
-public final class CowinApp {
-
-    public static String handleRequest(Map<String, String> input, Context context) {
-        context.getLogger().log("age passed:" + input.get("age"));
-        context.getLogger().log("user-agent passed:" + input.get("user-agent"));
-
-        if (!input.containsKey("age")) return "";
-
-        int age = Integer.parseInt(input.get("age"));
-        String userAgent = input.get("user-agent");
-
-        CowinController cowin = new CowinController();
-        return cowin.getAllAvailableSlots(age, userAgent).toString();
-    }
-
+@SpringBootApplication
+public class CowinApp {
     public static void main(String... s) {
-        CowinController cowin = new CowinController();
-        cowin.getAllAvailableSlots(45, "custom").toString();
+        SpringApplication.run(CowinApp.class);
     }
 
 
